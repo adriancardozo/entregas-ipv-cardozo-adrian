634101bafaf06a2b2be5fede88bad617a8ceda62
Adrián Cardozo <adrian.cardozo1@alu.unq.edu.ar>
2021-09-14 03:09:34 -0300

    Se agregaron controles a la interfaz

--- entrega-1-cardozo-adrian-extra/Entities/Mob.gd
+++ entrega-1-cardozo-adrian-extra/Entities/Mob.gd
@@ -1,7 +1,7 @@
 extends RigidBody2D
 
-export var min_speed = 150.0
-export var max_speed = 250.0
+export var min_speed = 150
+export var max_speed = 250
 
 func _ready():
 	var mob_types = $AnimatedSprite.frames.get_animation_names()
