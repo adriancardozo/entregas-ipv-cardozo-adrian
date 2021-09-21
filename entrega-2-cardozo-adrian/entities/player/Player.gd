b2cc75e2318df4080194da371bf330b38cb53e99
Adrián Cardozo <adrian.cardozo1@alu.unq.edu.ar>
2021-09-21 04:59:00 -0300

    Se completó la entrega y se agregó MRUV al player

--- entrega-2-cardozo-adrian/entities/player/Player.gd
+++ entrega-2-cardozo-adrian/entities/player/Player.gd
@@ -1,18 +1,29 @@
 extends Sprite
 
+onready var cannon: Sprite = $Cannon
+var velocity: Vector2 = Vector2.ZERO
+export (float) var ACCELERATION: float = 20.0
+export (float) var H_SPEED_LIMIT: float = 600.0 
+export (float) var FRICTION_WEIGHT: float = 0.1
+
 var speed = 200 #Pixeles
+var projectile_container: Node
+
+func set_projectile_container(container: Node):
+	cannon.projectile_container = container
+	projectile_container = container
 
 func _physics_process(delta):
-	# Manera básica
-	var direction:int = 0
-	if Input.is_action_pressed("move_left"):
-		direction = -1
-	elif Input.is_action_pressed("move_right"):
-		direction = 1
 	
-	#position.x += direction * speed * delta
+	var mouse_position: Vector2 = get_global_mouse_position()
+	cannon.look_at(mouse_position)
 	
-	# Manera optimizada
-	var direction_optimized:int = int(Input.is_action_pressed("move_right")) - int(Input.is_action_pressed("move_left"))
-	
-	position.x += direction_optimized * speed * delta
+	if Input.is_action_just_pressed("fire"):
+		cannon.fire()
+		
+	var direction:int = int(Input.is_action_pressed("move_right")) - int(Input.is_action_pressed("move_left"))
+	if direction != 0:
+		velocity.x = clamp(velocity.x + (direction * ACCELERATION), -H_SPEED_LIMIT, H_SPEED_LIMIT)
+	else:
+		velocity.x = lerp(velocity.x, 0, FRICTION_WEIGHT) if abs(velocity.x) > 1 else 0
+	position += velocity * delta
